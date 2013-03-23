144802839854aef8f3842abfb0d5cfcf37de8464
Joshua Tokle <jtokle@gmail.com>
2013-03-23 14:22:32 -0400

    output additional data values in guile version

--- guile/get_params_scm.c
+++ guile/get_params_scm.c
@@ -7,6 +7,7 @@ read_config(void *nothing) {
     scm_c_primitive_load("param_file_desc.scm");
     scm_c_primitive_load("params.scm");
 
+    // print the data directory
     SCM config = scm_variable_ref(scm_c_lookup("config"));
     SCM symbol = scm_from_locale_symbol("directory");
     SCM directory = scm_assq_ref(config, symbol);
@@ -14,17 +15,28 @@ read_config(void *nothing) {
     scm_dynwind_free(c_directory);
     printf("directory: %s\n", c_directory);
 
-    symbol = scm_from_locale_symbol("filea");
-    SCM filea = scm_assq_ref(config, symbol);
-    char *c_filea = scm_to_locale_string(filea);
-    scm_dynwind_free(c_filea);
-    printf("filea: %s\n", c_filea);
+    // print the value of the summary flag
+    symbol = scm_from_locale_symbol("summary");
+    SCM summary = scm_assq_ref(config, symbol);
+    printf("summary: %d\n", scm_is_true(summary));
 
-    symbol = scm_from_locale_symbol("fileb");
-    SCM fileb = scm_assq_ref(config, symbol);
-    char *c_fileb = scm_to_locale_string(fileb);
-    scm_dynwind_free(c_fileb);
-    printf("fileb: %s\n", c_fileb);
+    // print the name, length, and column parameters for the first field
+    SCM fields = scm_variable_ref(scm_c_lookup("config-fields"));
+    SCM f = scm_list_ref(fields, scm_from_int(0));
+    symbol = scm_car(f);
+    char *c_name = scm_to_locale_string(scm_symbol_to_string(symbol));
+    printf("%s %d %d %d\n", c_name,
+                            scm_to_int(scm_list_ref(f, scm_from_int(1))),
+                            scm_to_int(scm_list_ref(f, scm_from_int(2))),
+                            scm_to_int(scm_list_ref(f, scm_from_int(3))));
+    free(c_name);
+
+    // print the number of blocking fields in the first strategy
+    SCM strategies = scm_variable_ref(scm_c_lookup("config-strategies"));
+    SCM strategy = scm_list_ref(strategies, scm_from_int(0));
+    symbol = scm_from_locale_symbol("block");
+    SCM list = scm_assq_ref(strategy, symbol);
+    printf("number of blocking fields: %d\n", scm_to_int(scm_length(list)));
 
     scm_dynwind_end();
     return NULL;
